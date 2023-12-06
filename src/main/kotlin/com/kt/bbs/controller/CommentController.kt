74061c964b183511ba8acef20058c4604dfc362e
Hms200 <96504521+Hms200@users.noreply.github.com>
2023-12-06 14:13:51 +0900

    modify controller methods

--- src/main/kotlin/com/kt/bbs/controller/CommentController.kt
+++ src/main/kotlin/com/kt/bbs/controller/CommentController.kt
@@ -2,24 +2,32 @@ package com.kt.bbs.controller
 
 import com.kt.bbs.controller.dto.CommentCreateRequest
 import com.kt.bbs.controller.dto.CommentUpdateRequest
+import com.kt.bbs.service.CommentService
+import com.kt.bbs.service.dto.toDto
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class CommentController {
+class CommentController(
+    private val commentService: CommentService,
+) {
 
     @PostMapping("/posts/{postId}/comments")
     fun createComment(
         @PathVariable postId: Long,
         @RequestBody commentCreateRequest: CommentCreateRequest,
     ): Long {
-        println(commentCreateRequest.createdBy)
-        println(commentCreateRequest.content)
-        return 0L
+        return commentService.createComment(
+            commentCreateRequest.toDto(
+                postId = postId,
+                request = commentCreateRequest
+            )
+        )
     }
 
     @PutMapping("/comments/{commentId}")
@@ -27,15 +35,19 @@ class CommentController {
         @PathVariable commentId: Long,
         @RequestBody commentUpdateRequest: CommentUpdateRequest,
     ): Long {
-        println(commentUpdateRequest.updatedBy)
-        println(commentUpdateRequest.content)
-        return 0L
+        return commentService.updateComment(
+            commentUpdateRequest.toDto(commentId)
+        )
     }
 
     @DeleteMapping("/comments/{commentId}")
     fun deleteComment(
         @PathVariable commentId: Long,
+        @RequestParam deletedBy: String,
     ): Long {
-        return 0L
+        return commentService.deleteComment(
+            commentId = commentId,
+            deletedBy = deletedBy,
+        )
     }
 }
--- src/main/kotlin/com/kt/bbs/service/dto/CommentUpdateRequestDto.kt
+++ src/main/kotlin/com/kt/bbs/service/dto/CommentUpdateRequestDto.kt
@@ -1,7 +1,17 @@
 package com.kt.bbs.service.dto
 
+import com.kt.bbs.controller.dto.CommentUpdateRequest
+
 data class CommentUpdateRequestDto(
     val commentId: Long,
     val content: String,
     val updatedBy: String,
 )
+
+fun CommentUpdateRequest.toDto(commentId: Long): CommentUpdateRequestDto {
+    return CommentUpdateRequestDto(
+        commentId = commentId,
+        content = this.content,
+        updatedBy = this.updatedBy,
+    )
+}
